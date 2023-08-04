56902b4bea5f1b1babf4e52f2483658f2de6a895
Brayan Bejarano <brayanb1995@gmail.com>
2023-08-03 19:19:06 -0500

    index add funtion home

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import React from 'react'
+
+
+function Home(){
+    const listItems = [1,2,3,4]
+    
+    const addItem = () -> {
+        const varibleA = 1;
+        const varibleB = 3;
+        const varibleC = 2;
+
+        return varibleA*3 + varibleC*varibleB
+    }
+    return(
+        <> <div>Home</div> <button onclick={addItem}/></>
+    )
+}
+
+
+export default Home
